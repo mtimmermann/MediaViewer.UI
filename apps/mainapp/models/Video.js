0ffd154aceaffd8e626b0b6e5fd2b06ed49958ca
Mark Timmermann <mtimmermann@hotmail.com>
2013-12-17 20:26:50 -0700

    Formatting duration to HH:MM:SS

--- apps/mainapp/models/Video.js
+++ apps/mainapp/models/Video.js
@@ -17,7 +17,8 @@ define(function(require, exports, module) {
             data.durationFormatted = '';
             if (data.meta && data.meta.format &&
                 typeof data.meta.format.duration === 'number') {
-                    data.durationFormatted = data.meta.format.duration.toFixed(2);
+                    data.durationFormatted = this._secsToHHMMSS(
+                        parseInt(data.meta.format.duration.toFixed(0), 10));
             }
 
             if (!data.meta) {
@@ -44,6 +45,21 @@ define(function(require, exports, module) {
                 required: true,
                 msg: 'A video file is required'
             }
+        },
+
+        // TODO: Move to shared utility if this is needed elswhere
+        _secsToHHMMSS: function(secondsTotal) {
+            if (typeof secondsTotal !== 'number') { throw (new Error('_secsToHHMMSS: secondsTotal param must be a number')); }
+            //var sec_num = parseInt(this, 10); // don't forget the second parm
+            var hours   = Math.floor(secondsTotal / 3600);
+            var minutes = Math.floor((secondsTotal - (hours * 3600)) / 60);
+            var seconds = secondsTotal - (hours * 3600) - (minutes * 60);
+
+            if (hours   < 10) {hours   = "0"+hours;}
+            if (minutes < 10) {minutes = "0"+minutes;}
+            if (seconds < 10) {seconds = "0"+seconds;}
+            var time    = hours+':'+minutes+':'+seconds;
+            return time;
         }
 
     });
